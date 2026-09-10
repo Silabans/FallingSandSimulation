f50f9269dd98c3e27efcae497bd0e7941dc9f46f
Matthew <matthew.yedija@gmail.com>
2026-09-10 14:47:55 +0700

    colour shifting waters using sin squared formula

--- grid.hpp
+++ grid.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "tools.hpp"
 #include "materials.hpp"
+#include <algorithm>
 #include <utility> // for std::swap
 
 
@@ -10,6 +11,7 @@ struct Cell {
     uint8_t life = 15; // fire lifetime, acid strength, etc
     uint8_t lastFrame = 0; // prevents double updating cell in one iteration
     int speed; // for an accelerating fall
+    float colorOffset;
 };
 
 
@@ -44,6 +46,24 @@ bool inBounds(int x, int y) {
     return x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT;
 }
 
+// helper function for ColorLerp
+unsigned char UnsignedClamp(float val) {
+    val = std::clamp(val, 0.0f, 255.0f);
+    return static_cast<unsigned char>(val);
+}
+
+// linear interpolation: for smooth blend between two colours (based on parameter t, where 0.0f <= t <= 1.0f)
+Color inline ColorLerp(const Color& a, const Color& b, float t) {
+    t = std::clamp(t, 0.0f, 1.0f);
+
+    return Color{
+        UnsignedClamp((float)a.r + ((float)(b.r - a.r) * t)),
+        UnsignedClamp((float)a.g + ((float)(b.g - a.g) * t)),
+        UnsignedClamp((float)a.b + ((float)(b.b - a.b) * t)),
+        255
+    };
+}
+
 
 bool isEmpty(int x, int y) {
     if (!inBounds(x, y)) return false; // the boundaries are static walls (not empty)
@@ -53,7 +73,7 @@ bool isEmpty(int x, int y) {
 
 void ResolveFire(int x, int y, Cell& cell) {
     if (cell.life > 0) {
-        if (GetRandomValue(1, 100) > 15) cell.life--;
+        if (GetRandomValue(1, 100) > 50)cell.life--;
     } else {
         // Turn dead fire into smoke with a high probability
         if (GetRandomValue(1, 100) <= 70) {
--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "game.hpp"
 #include <cstdlib>
 
+
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Falling Sand Engine");
     SetTargetFPS(60);
 
     InitGrid();
+
+    static Color FIRE_LUT[31]; // Look-up Table for fire
+    static bool lut_initialised = false;
+
+    if (!lut_initialised) {
+        const Color FIRE_CORE  = { 255, 240, 150, 255 }; // Bright Yellow / White Core
+        const Color FIRE_MID   = { 255, 165, 0, 255 }; // Deep Orange
+        const Color FIRE_EMBER = { 150, 20, 0, 255 }; // Dark Red
+
+        for (int life = 0; life <= 30; ++life) {
+            float lifeRatio = life > 30 ? 1.0f : static_cast<float>(life) / 30.0f;
+            if (lifeRatio > 0.5f) {
+                float t = (1.0f - lifeRatio) * 2.0f;
+                FIRE_LUT[life] = ColorLerp(FIRE_CORE, FIRE_MID, t);
+            } else {
+                float t = (0.5f - lifeRatio) * 2.0f;
+                FIRE_LUT[life] = ColorLerp(FIRE_MID, FIRE_EMBER, t);
+            }
+        }
+        lut_initialised = true;
+    }
 
     Color* pixels = (Color*)malloc(WIDTH * HEIGHT * sizeof(Color)); // allocate a block of memory for the pixels
     Image img = GenImageColor(WIDTH, HEIGHT, BLACK); //CPU memory
@@ -16,11 +38,32 @@ int main() {
         UpdateSimulation();
         UserInt(15);
 
+        // For water color shift
+        static float frequency = 1.0f / 300.0f;
+        float water_t = (std::sin((float)currentFrame * frequency * PI)) * (std::sin((float)currentFrame * frequency * PI));
+
         for (int i = 0; i < WIDTH * HEIGHT; ++i) {
-            ElementType t = grid[i].type;
-            pixels[i] = ELEMENT_REGISTRY[static_cast<int>(t)].color;
+            Cell& cell = grid[i];
+            ElementType type = cell.type;
+            ElementProperties props = ELEMENT_REGISTRY[static_cast<int>(type)];
+
+            if (type == ElementType::FIRE) {
+                pixels[i] = FIRE_LUT[cell.life];
+            } 
+            else if (type == ElementType::WATER) {
+                static Color hueShift = {0, 0, 160, 255};
+                pixels[i] = ColorLerp(props.color, hueShift, water_t);
+            } 
+            else {
+                float factor = 1.0f - ((float)cell.colorOffset / 255.0f) * 0.1f;
+                Color color = props.color;
+                color.r = color.r * factor;
+                color.g = color.g * factor;
+                color.b = color.b * factor;
+                pixels[i] = color;
+            }
         }
-        UpdateTexture(texture, pixels); // Copies the whole batch of raw bytes from the CPU RAM into
+        UpdateTexture(texture, pixels); // Copies the whole batch of raw bytes from the CPU RAM intod
         // into the GPU's VRAM texture buffer
 
         BeginDrawing();
